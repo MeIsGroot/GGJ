eb8b70d4c17507ece1d4f83b848693d335939d56
Satwik Gorla <st325640@dal.ca>
2025-01-25 14:02:52 -0400

    feat: added title screen functionality

---
+++ BubbleDucky/start_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$BubbleDucky.play
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+#Button press calls
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://main_game.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://settings.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
